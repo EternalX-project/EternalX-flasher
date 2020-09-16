4328ffd01be5604eea8c1df24780802f25eaf38d
zexceed12300 <zexceed12300@gmail.com>
2020-09-16 13:22:37 +0000

    merge kernel.string with EternalX UI

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=#           EternalX Kernel            #
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=rosy
+device.name2=
+device.name3=
+device.name4=
 device.name5=
-supported.versions=
+supported.versions=8 - 10
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
